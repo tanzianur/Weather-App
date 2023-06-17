7365ba4ae420d0d9e10bd7e8ec51a1d783caf5c1
Mehdi Imam <mehdiimam439@gmail.com>
2023-06-17 18:43:02 -0500

    Style WeatherSearch component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import WeatherSearch from "./components/search";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p>
-      </header>
       <WeatherSearch /> {/* Include the WeatherSearch component */}
     </div>
   );
